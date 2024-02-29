b7d6da3765cb80d7cecdd0a95dc11ade3544b6dc
Farah065 <farahahmad065@gmail.com>
2024-03-01 01:33:27 +0200

    implemented metadata writing

--- src/main/java/org/example/Table.java
+++ src/main/java/org/example/Table.java
@@ -1,13 +1,31 @@
 package org.example;
 
+import com.opencsv.CSVWriter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Table {
     String[] fileNames;//pages
     String[] columnNames;
     Page currentPage;
 
     //store relevant info about pages and serialize it
-    public Table( String tableName, String[] columnName, String[] columnType, boolean[]clusteringKey, String[] indexName, String[] indexType){
+    public Table( String tableName, String[] columnName, String[] columnType, boolean[] clusteringKey, String[] indexName, String[] indexType){
         //writeMetadataToCSV();
+            CSVWriter writer = new CSVWriter(DBApp.outputFile, CSVWriter.DEFAULT_SEPARATOR, CSVWriter.NO_QUOTE_CHARACTER,
+                    CSVWriter.DEFAULT_ESCAPE_CHARACTER, CSVWriter.RFC4180_LINE_END);
+
+            for(int i = 0; i < columnName.length; i++){
+                String[] info = {tableName,
+                                 columnName[i],
+                                 columnType[i],
+                                 clusteringKey[i] ? "True" : "False",
+                                 indexName[i],
+                                 indexType[i]};
+                writer.writeNext(info);
+            }
         //Loop on tuple and insert in form
         // CityShop, ID, java.lang.Integer, True, IDIndex, B+tree
         this.columnNames = columnName;
