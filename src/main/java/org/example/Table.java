0f7377c36212d99d26f3a69212de60fac08c119c
Farah065 <farahahmad065@gmail.com>
2024-03-08 15:20:25 +0200

    Fixed metadata file

--- src/main/java/org/example/Table.java
+++ src/main/java/org/example/Table.java
@@ -10,30 +10,33 @@ import java.util.Hashtable;
 import java.util.Enumeration;
 
 public class Table implements Serializable {
-    String[] fileNames;//pages
-    String[] columnNames;
+    String tableName;
+    Hashtable<String,String> ColNameType;
+    String clusteringKey;
+    Hashtable<String, String> indicesColNameType;
     Page currentPage;
 
     //store relevant info about pages and serialize it
-    public Table(String tableName, Hashtable<String,String> ColNameType, String clusteringKey, String[] indexName, String[] indexType){
-        //writeMetadataToCSV();
-            CSVWriter writer = new CSVWriter(DBApp.outputFile, CSVWriter.DEFAULT_SEPARATOR, CSVWriter.NO_QUOTE_CHARACTER,
-                    CSVWriter.DEFAULT_ESCAPE_CHARACTER, CSVWriter.RFC4180_LINE_END);
-        Enumeration<String> e = ColNameType.keys();
+    public Table(String tableName, String clusteringKey, Hashtable<String,String> colNameType){
+
+        this.tableName = tableName;
+        this.ColNameType = colNameType;
+        this.clusteringKey = clusteringKey;
+
+        CSVWriter writer = new CSVWriter(DBApp.outputFile, CSVWriter.DEFAULT_SEPARATOR, CSVWriter.NO_QUOTE_CHARACTER,
+                CSVWriter.DEFAULT_ESCAPE_CHARACTER, CSVWriter.RFC4180_LINE_END);
+        Enumeration<String> e = colNameType.keys();
         while (e.hasMoreElements()){
             String key = e.nextElement();
             String[] info = {tableName,
-                             key,
-                             ColNameType.get(key),
-                             clusteringKey==key ? "True" : "False",
-                             null,
-                             null};
+                    key,
+                    colNameType.get(key),
+                    clusteringKey==key ? "True" : "False",
+                    "null",
+                    "null"};
+
             writer.writeNext(info);
         }
-
-        //Loop on tuple and insert in form
-        // CityShop, ID, java.lang.Integer, True, IDIndex, B+tree
-
     }
     public void createPage(){
         //when inserting
@@ -42,8 +45,8 @@ public class Table implements Serializable {
         //insert and update index
         //boolean isInsertCorrectly = currentPage.insert(tuple);
         //if (!isInsertCorrectly){
-            //AddToPage(newPage);
-            //logic for sorting
+        //AddToPage(newPage);
+        //logic for sorting
         //}
 
     }
