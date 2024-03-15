410318cf79058a54d105620cfb66ab025e274b1e
omar-yasser-ayoub <omar.ayoub@student.guc.edu.eg>
2024-03-15 16:48:30 +0200

    remove validity checks from SQLTerm Constructor, replace with default constructor and constructor that takes the values

--- src/main/java/org/example/SQLTerm.java
+++ src/main/java/org/example/SQLTerm.java
@@ -14,67 +14,13 @@ public class SQLTerm {
 	public String _strTableName,_strColumnName, _strOperator;
 	public Object _objValue;
 
-	public SQLTerm(String strTableName, String strColumnName, String strOperator, Object objValue) throws DBAppException, IOException, CsvValidationException {
-		// get datatype of value
-		String type = "";
-		if (objValue instanceof String) {
-			type = "java.lang.String";
-		}
-		else if (objValue instanceof Integer) {
-			type = "java.lang.Integer";
-		}
-		else if (objValue instanceof Double) {
-			type = "java.lang.Double";
-		}
-
-		try {
-			// create a reader
-			CSVReader reader = new CSVReader(new FileReader("src/main/java/org/example/resources/metadata.csv"));
-			String[] line = reader.readNext();
-
-			// exception message stuff
-			String exceptionMsg = "exceptionMsg";
-			Boolean colInTable = false;
-			Boolean tableExists = false;
-
-			while((line = reader.readNext()) != null){
-				if(strTableName.equals(line[0]) && strColumnName.equals(line[1])){
-					exceptionMsg = "Object datatype doesn't match column datatype";
-					colInTable = true;
-					tableExists = true;
-				}
-				if(strTableName.equals(line[0]) && !strColumnName.equals(line[1]) && !colInTable){
-					exceptionMsg = "Column does not exist in table";
-					tableExists = true;
-				}
-				if(!tableExists)
-					exceptionMsg = "Table does not exist";
-
-				// if table exists and contains columns, and the datatype matches the column datatype, assign the values
-				if(strTableName.equals(line[0]) && strColumnName.equals(line[1]) && type.equals(line[2])){
-					_strTableName = strTableName;
-					_strColumnName = strColumnName;
-					_objValue = objValue;
-					break;
-				}
-			}
-
-			// if values were never assigned, that means condition was never met and exception should be thrown
-			if(_strTableName == null)
-				throw new DBAppException(exceptionMsg);
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-		catch (CsvValidationException e) {
-			e.printStackTrace();
-		}
-
-		// check if operator is valid
-		if(strOperator == ">" || strOperator == ">=" || strOperator == "<" || strOperator == "<=" || strOperator == "!=" || strOperator == "=")
-			_strOperator = strOperator;
-		else
-			throw new DBAppException("Illegal operator");
+	public SQLTerm(String strTableName, String strColumnName, String strOperator, Object objValue) {
+		_strTableName = strTableName;
+		_strColumnName = strColumnName;
+		_objValue = objValue;
+		_strOperator = strOperator;
 	}
+	public SQLTerm() {
 
+	}
 }
